fb7b140cf873e77b4b4ce3323da976a43df661c9
Panagiotis <pgiagkoulas@gmail.com>
2019-06-01 22:36:38 +0200

    Revert "Revert "added configuration/hyperparameter support""
    This reverts commit 84ed81d2a560ebd2550fb2a84288f88931baa13a.

--- FunctionalConvLstm.py
+++ FunctionalConvLstm.py
@@ -10,17 +10,30 @@ from keras.layers import Input, Embedding, LSTM, Dense
 from keras.models import Model
 
 
-def evaluate_multi_model(trainX, trainy, testX, testy, aux_trainX, aux_trainy, aux_testX, aux_testy):
-    # define model
-    verbose, epochs, batch_size = 0, 25, 64
+def evaluate_multi_model(trainX, trainy, testX, testy, aux_trainX, aux_trainy, aux_testX, aux_testy, cfg):
+    ## datastuff
     n_timesteps, n_features, n_outputs = trainX.shape[1], trainX.shape[2], trainy.shape[1]
+    n_aux_features = aux_trainX.shape[1]
     # reshape into subsequences (samples, time steps, rows, cols, channels)
     n_steps, n_length = 4, 32
     trainX = trainX.reshape((trainX.shape[0], n_steps, 1, n_length, n_features))
-    # trainX = trainX
-    # trainy = trainy
     testX = testX.reshape((testX.shape[0], n_steps, 1, n_length, n_features))
-    # define model
+    verbose, epochs, batch_size = 0, 25, 64
+    trainX = trainX[:100]
+    trainy = trainy[:100]
+    aux_trainX = aux_trainX[:100]
+    aux_trainy = aux_trainy[:100]
+    
+    ## parameterstuff
+    verbose = cfg.get('verbose') if ('verbose' in cfg) else 0
+    epochs = cfg.get('epochs') if ('epochs' in cfg) else 25
+    batch_size = cfg.get('batch_size') if ('batch_size' in cfg) else 64
+    #print(verbose)
+    #print(epochs)
+    #print(batch_size)
+	#verbose, epochs, batch_size): = 0, 25, 64
+    
+    ## modelstuff
     main_input = Input(shape=(n_steps, 1, n_length, n_features), dtype='float32', name='main_input')
     lstm_out = ConvLSTM2D(filters=64, kernel_size=(1,3), activation='relu')(main_input)
     x = Dropout(rate=0.5)(lstm_out)
@@ -34,11 +47,9 @@ def evaluate_multi_model(trainX, trainy, testX, testy, aux_trainX, aux_trainy, a
     # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     # model.fit(trainX, trainy, epochs=epochs, batch_size=batch_size, verbose=0)
 
-    # auxiliary input shape
-    num_features = aux_trainX.shape[1]
     # flatten output
     lstm_out_flat = Flatten()(lstm_out)
-    auxiliary_input = Input(shape=(num_features,), name='aux_input')
+    auxiliary_input = Input(shape=(n_aux_features,), name='aux_input')
     # combine inputs
     x = Concatenate()([lstm_out_flat, auxiliary_input])
     # rest of the network
@@ -49,8 +60,13 @@ def evaluate_multi_model(trainX, trainy, testX, testy, aux_trainX, aux_trainy, a
     main_output = Dense(6, activation='softmax', name='main_output')(x)
     model = Model(inputs=[main_input, auxiliary_input], outputs=[main_output, auxiliary_output])
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    
+    ## train/test
     model.fit(x=[trainX, aux_trainX], y=[trainy, aux_trainy], epochs=epochs, batch_size=batch_size, verbose=verbose)
     #print(model.metrics_names)
     #print(model.summary())
     _, _, _, accuracy, aux_acc = model.evaluate(x=[testX, aux_testX], y=[testy, aux_testy], batch_size=batch_size, verbose=1)
     return accuracy, aux_acc
+    
+    
